d661a8b4e74800ea215826e15d6a5836b8ad95c8
Shubham Srivastava <mohan.shubham0107@gmail.com>
2025-09-14 00:29:06 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull shubham010701/onlineshop:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 shubham010701/onlineshop:latest
